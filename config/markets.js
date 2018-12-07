446ac64a77e39f361eed8e1a3aad787da3943b00
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-12-07 11:10:18 +0100

    Markets constant

---
+++ config/markets.js
@@ -0,0 +1,60 @@
+export default {
+	"au": {
+		country: "Australia",
+		url: "https://www.amazon.com.au"
+	},
+	"br": {
+		country: "Brazil",
+		url: "https://www.amazon.com.br"
+	},
+	"ca": {
+		country: "Canada",
+		url: "https://www.amazon.ca"
+	},
+	"cn": {
+		country: "China",
+		url: "https://www.amazon.cn"
+	},
+	"fr": {
+		country: "France",
+		url: "https://www.amazon.fr"
+	},
+	"de": {
+		country: "Germany",
+		url: "https://www.amazon.de"
+	},
+	"in": {
+		country: "India",
+		url: "https://www.amazon.in"
+	},
+	"it": {
+		country: "Italy",
+		url: "https://www.amazon.it"
+	},
+	"jp": {
+		country: "Japan",
+		url: "https://www.amazon.co.jp"
+	},
+	"mx": {
+		country: "Mexico",
+		url: "https://www.amazon.com.mx"
+	},
+	"es": {
+		country: "Spain",
+		url: "https://www.amazon.es"
+	},
+	"tr": {
+		country: "Turkey",
+		url: "https://www.amazon.com.tr"
+	},
+	"uk": {
+		country: "United Kingdom",
+		url: "https://www.amazon.co.uk"
+	},
+	"com": {
+		country: "United States",
+		url: "https://www.amazon.com"
+	},
+
+
+}
